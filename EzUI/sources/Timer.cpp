43fda7d639e3133eefb742f936eb1c0c9eb80031
ly <ly@DESKTOP-TSUJJL5>
2023-08-23 00:51:55 +0800

    忘记加lib引用了 补上

--- EzUI/sources/Timer.cpp
+++ EzUI/sources/Timer.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Timer.h"
+#pragma comment(lib,"Winmm.lib")
 namespace EzUI {
 	namespace Thread {
 		Timer::Timer() {}
--- kugou/main.cpp
+++ kugou/main.cpp
@@ -1,7 +1,6 @@
 #include "global.h"
 #include "MainFrm.h"
 
-#pragma comment(lib,"Winmm.lib")
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
