ac7f46e6782aca651325739054f312985d02eabf
yang <yang@DESKTOP-NV4IFCD>
2025-07-19 20:16:25 +0800

    写上点注释

--- demo/kugou/main.cpp
+++ demo/kugou/main.cpp
@@ -6,41 +6,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-
 	Curl_Global_Init();//初始化curl
-	Application app;
-	app.SetResource("my_res");
-
+	Application app;//app类
 	app.EnableHighDpi();//启用高dpi适配
+	app.SetResource("my_res");//设定资源名称
 
-	//while (true)
-	//{
-	//	VListView view;
-	//	for (size_t i = 0; i < 999; i++)
-	//	{
-	//		//btn无需释放 生命周期跟随view
-	//		Button* btn = new Button(&view);
-	//		btn->SetFixedSize({ 100,30 });
-	//		btn->SetText(L"按钮1");
-	//		view.Add(btn);
-	//	}
-	//	int pause = 0;
-	//}
-
-	MainFrm frm;
+	MainFrm frm;//主窗口
 
 	//给窗口添加淡入效果
-	Animation* ant = new Animation(&frm);
+	Animation* ant = new Animation(&frm);//绑定父对象为frm,则ant无需手动释放
 	ant->SetStartValue(0.1);
 	ant->SetEndValue(1.0);
 	ant->ValueChanged = [&](double value) {
 		Invoke([&] {
-			frm.Opacity = value;
-			frm.Invalidate();
+			frm.Opacity = value;//修改透明度
+			frm.Invalidate();//刷新
 			});
 		};
 	ant->Start(200);//开始动画
 
 	frm.Show();
+
 	return app.Exec();
 }
